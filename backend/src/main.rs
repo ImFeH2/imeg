49d0ac7abafa0d07a9ed392298c2efe26c75315c
imfeh2 <i@feh2.im>
2024-11-21 16:58:26 +0800

    chore: initialize React project with TypeScript and Tailwind CSS

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,5 @@
-mod error;
 mod converter;
+mod error;
 mod utils {
     pub mod file;
 }
@@ -11,10 +11,10 @@ use std::path::Path;
 use utils::file::markdown_paths;
 
 fn main() -> Result<(), ConversionError> {
-    if Path::new("html").exists() {
-        fs::remove_dir_all("html")?;
+    if Path::new("../../html").exists() {
+        fs::remove_dir_all("../../html")?;
     }
-    fs::create_dir("html")?;
+    fs::create_dir("../../html")?;
 
     let markdown_files = markdown_paths("markdown")?;
 
@@ -23,11 +23,11 @@ fn main() -> Result<(), ConversionError> {
     options.compile.allow_dangerous_protocol = true;
 
     for markdown_path in markdown_files {
-        convert_file(&markdown_path, Some(Path::new("html")), &options)?;
+        convert_file(&markdown_path, Some(Path::new("../../html")), &options)?;
     }
 
-    let readme = Path::new("README.md");
-    convert_file(readme, Some(Path::new("html")), &options)?;
+    let readme = Path::new("../../README.md");
+    convert_file(readme, Some(Path::new("../../html")), &options)?;
 
     println!("Successfully converted all markdown files to HTML!");
     Ok(())
